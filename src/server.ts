ec65abefa9128d2e0226ad32cf5f9e831340c462
= <=>
2023-12-21 18:09:38 +0530

    fix: added file permissions

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,15 @@ import { postHandler } from './handlers/postHandler';
 import { masking } from './middleware/masking';
 import { cors } from './middleware/cors';
 import path from 'path'
+import {execSync} from 'child_process'
 
 const app = express()
 app.use(express.json())
 app.use(cors)
+
+execSync('chmod 777 /tmp/image-file.png')
+
+console.log('Added file permissions')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -18,6 +23,7 @@ const storage = multer.diskStorage({
     }
 });
 
+console.log('Sending data to handler')
 const upload = multer({storage})
 
 app.post('/', upload.single('imageFile'), postHandler)
